80948dde29055ac8c8c8a035f403ad007afb0ffb
18159761 <48758161+18159761@users.noreply.github.com>
2020-03-26 18:02:55 +0000

    New changes
    Got rid of errors

--- app/src/main/java/ie/ul/tutorfinder/MainActivity.java
+++ app/src/main/java/ie/ul/tutorfinder/MainActivity.java
@@ -1,15 +1,19 @@
 package ie.ul.tutorfinder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.text.Collator;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,19 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
     public void onClickSignIn(View view) {
         List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.idpConfig.EmailBuilder().build()
+                new AuthUI.IdpConfig.EmailBuilder().build()
         );
 
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
-                        .setAvailibleProviders( providers )
+                        .setAvailableProviders( providers )
                         .build(), RC_SIGN_IN );
 
     }
@@ -39,10 +43,10 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult( requestCode, resultCode, data );
 
         if (requestCode == RC_SIGN_IN) {
-            idpResponse response = idpResponse.fromResultIntent( data );
+            IdpResponse response = IdpResponse.fromResultIntent( data );
 
             if (resultCode == RESULT_OK) {
-                FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 System.out.println( "Sign in Successful! \n" +
                         "name = " + user.getDisplayName() + "\n" +
                         "email = " + user.getEmail() + "\n" +
