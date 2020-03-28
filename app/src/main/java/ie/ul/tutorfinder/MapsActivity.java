ab3b89465d77da98c0e3ea7181d1d0d3cd006e54
18159761 <48758161+18159761@users.noreply.github.com>
2020-03-28 05:25:23 +0000

    Map
    Added the map functionality for the the "Find tutor" function within the main menu of the account holder.

---
+++ app/src/main/java/ie/ul/tutorfinder/MapsActivity.java
@@ -0,0 +1,39 @@
+package ie.ul.tutorfinder;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import ie.ul.tutorfinder.R;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng university = new LatLng(52.673551, -8.572256
+        );
+        mMap.addMarker(new MarkerOptions().position(university).title("Marker in university"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(university));
+    }
+}
