c7359dec267fb0961744132f409cd7ab83112bd5
18159761 <48758161+18159761@users.noreply.github.com>
2020-03-29 20:33:30 +0100

    login system
    Login authetification created
    and connects with firebase

---
+++ app/src/main/java/ie/ul/tutorfinder/LoginActivity.java
@@ -0,0 +1,63 @@
+package ie.ul.tutorfinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    Toolbar toolbar;
+    EditText userPass;
+    EditText userEmail;
+    Button userLogin;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_login );
+
+        userEmail = findViewById( R.id.etUserEmail );
+        userPass = findViewById( R.id.etUserPass );
+        userLogin = findViewById( R.id.btnUserLogin );
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        userLogin.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signInWithEmailAndPassword( userEmail.getText().toString(),
+                        userPass.getText().toString() )
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    startActivity( new Intent( LoginActivity.this, MainUserScreen.class ) );
+                                } else {
+                                    Toast.makeText( LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG ).show();
+                                }
+
+                            }
+                        } );
+            }
+
+        } );
+    }
+}
+
+
--- app/src/main/java/ie/ul/tutorfinder/MessageActivity.java
+++ app/src/main/java/ie/ul/tutorfinder/MessageActivity.java
@@ -1,8 +1,8 @@
 package ie.ul.tutorfinder;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class MessageActivity extends AppCompatActivity {
 
