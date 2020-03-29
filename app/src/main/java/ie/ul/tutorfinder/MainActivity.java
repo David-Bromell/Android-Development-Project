efd18486267358ab61f37d8c5da08eadb09c6848
Archit Khanna <architk.222@gmail.com>
2020-03-29 19:06:49 +0100

    Revert "Merge branch 'Adrian' of https://github.com/David-Bromell/Android-Development-Project into Adrian"
    This reverts commit 790fa6abe60d03910d6bf5b345eb96c74f809f60, reversing
    changes made to 2736149886f773eba8241baf3658ba87c5ead13e.

--- app/src/main/java/ie/ul/tutorfinder/MainActivity.java
+++ app/src/main/java/ie/ul/tutorfinder/MainActivity.java
@@ -3,83 +3,83 @@ package ie.ul.tutorfinder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
-    Toolbar toolbar;
-    EditText email;
-    EditText password;
-    Button signup;
 
-    FirebaseAuth firebaseAuth;
+    private static final int RC_SIGN_IN = 123;
+
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public static String EXTRA_NAME = "ie.ul.tutorfinder.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
-        email = findViewById( R.id.etEmail );
-        password = findViewById( R.id.etPassword );
-        signup = findViewById( R.id.btnSignup );
-        login = findViewById( R.id.btnLogin );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        signup.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                firebaseAuth.createUserWithEmailAndPassword( email.getText().toString(), password.getText().toString() )
-                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText( MainActivity.this, "registered successfully!", Toast.LENGTH_LONG ).show();
-                                } else {
-                                    Toast.makeText( MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG ).show();
+    public void onClickSignIn(View view) {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
 
-                                }
-                            }
-                        } );
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders( providers )
+                        .build(), RC_SIGN_IN );
+
+    }
+
+    public void LoadUserPage(String name){
+        Intent intent = new Intent (this, MainUserScreen.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String name = editText.getText().toString();
+        intent.putExtra(EXTRA_NAME, name);
+        startActivity(intent);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult( requestCode, resultCode, data );
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent( data );
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String name = user.getDisplayName();
+                LoadUserPage(name);
+                System.out.println( "Sign in Successful! \n" +
+                        "name = " + user.getDisplayName() + "\n" +
+                        "email = " + user.getEmail() + "\n" +
+                        "id = " + user.getUid() );
+            } else {
+                if (response == null) {
+                    System.out.println( "Sign in cancelled" );
+                    return;
+                }
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    System.out.println( "No internet connection" );
+                    return;
+                }
+
             }
-        } );
 
-        login.setOnClickListener(new View.OnClickListener() {
+        }
 
-
-            @Override
-            public void onClick(View view) {
-                startActivity( new Intent(MainActivity.this, LoginActivity.class));
-            }
-        } );
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
