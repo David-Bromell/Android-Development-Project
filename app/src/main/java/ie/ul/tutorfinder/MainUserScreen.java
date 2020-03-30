5cd7453e8ef9d919c6104d14419c8e19b6364758
David-Bromell <44446626+David-Bromell@users.noreply.github.com>
2020-03-30 11:05:44 +0100

    Messanger Screen
    Messanger Screen created and linked to "Messages" button on User Screen.
    Note: messanging platform not yet functional, will do after work!

--- app/src/main/java/ie/ul/tutorfinder/MainUserScreen.java
+++ app/src/main/java/ie/ul/tutorfinder/MainUserScreen.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainUserScreen extends AppCompatActivity {
+
+    private Button MyMessagesBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,19 @@ public class MainUserScreen extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.textView);
         //textView.setText(name);
+        MyMessagesBtn = findViewById(R.id.MyMessagesBtn);
+        MyMessagesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMessangerActivity();
+
+            }
+        });
+
+    }
+
+    public void openMessangerActivity(){
+        Intent intent = new Intent(this, MessangerActivity.class);
+        startActivity(intent);
     }
 }
