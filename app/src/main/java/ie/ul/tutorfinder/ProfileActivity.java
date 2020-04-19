f55d66c1ecfff724bb7d0f0465c777ec92e3fddc
David-Bromell <44446626+David-Bromell@users.noreply.github.com>
2020-04-19 21:31:20 +0100

    Logout button
    Logout button reimplimented

--- app/src/main/java/ie/ul/tutorfinder/ProfileActivity.java
+++ app/src/main/java/ie/ul/tutorfinder/ProfileActivity.java
@@ -38,34 +38,34 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        logOut = findViewById( R.id.LogoutBtn );
+        logOut = findViewById(R.id.LogoutBtn);
 
         mauth = FirebaseAuth.getInstance();
         currentUserId = mauth.getCurrentUser().getUid();
-        userRef=FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId );
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
         //Intent intent  = getIntent();
-       // email = intent.getStringExtra( "email" );
+        // email = intent.getStringExtra( "email" );
 
-        FirstNameTextiView = findViewById( R.id.first_Name_TextView );
-        EmailTextView = findViewById( R.id.email_TextView );
-        PhoneTextView = findViewById( R.id.phone_TextView );
-        BirthDateTextView = findViewById( R.id.birthDate_TextView );
+        FirstNameTextiView = findViewById(R.id.first_Name_TextView);
+        EmailTextView = findViewById(R.id.email_TextView);
+        PhoneTextView = findViewById(R.id.phone_TextView);
+        BirthDateTextView = findViewById(R.id.birthDate_TextView);
 
 
-        userRef.addValueEventListener( new ValueEventListener() {
+        userRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                if(dataSnapshot.exists()) {
+                if (dataSnapshot.exists()) {
 
                     String userName = dataSnapshot.child("name").getValue().toString();
                     String email = dataSnapshot.child("email").getValue().toString();
                     String birthdate = dataSnapshot.child("birthdate").getValue().toString();
                     String phone = dataSnapshot.child("phone").getValue().toString();
 
-                FirstNameTextiView.setText("Welcome to your profile, " + userName +"!");
-                EmailTextView.setText("EMAIL: " + email);
-                PhoneTextView.setText("CONTACT NUM: " + phone);
+                    FirstNameTextiView.setText("Welcome to your profile, " + userName + "!");
+                    EmailTextView.setText("EMAIL: " + email);
+                    PhoneTextView.setText("CONTACT NUM: " + phone);
                     BirthDateTextView.setText("DOB: " + birthdate);
 
 
@@ -77,13 +77,19 @@ public class ProfileActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        } );
+        });
+
+        logOut.setOnClickListener(new View.OnClickListener() {
 
 
-
-
-
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity( new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
     }
+
 
 
 
