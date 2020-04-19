756af85ea8bbb2e715b3345701a91b361f930560
David-Bromell <44446626+David-Bromell@users.noreply.github.com>
2020-04-19 21:04:29 +0100

    Revert "Logout Button"
    This reverts commit ddc62232febb6ff3fe7df47494c5ad0b8095d42d.

--- app/src/main/java/ie/ul/tutorfinder/ProfileActivity.java
+++ app/src/main/java/ie/ul/tutorfinder/ProfileActivity.java
@@ -27,7 +27,7 @@ public class ProfileActivity extends AppCompatActivity {
     private FirebaseAuth mauth;
     private String currentUserId;
 
-    // private String email;
+   // private String email;
     //private static final String USERS =  "Users";
 
     Button logOut;
@@ -38,64 +38,54 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        logOut = findViewById(R.id.LogoutBtn);
-
+        logOut = findViewById( R.id.LogoutBtn );
 
         mauth = FirebaseAuth.getInstance();
         currentUserId = mauth.getCurrentUser().getUid();
-        userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+        userRef=FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId );
         //Intent intent  = getIntent();
-        // email = intent.getStringExtra( "email" );
+       // email = intent.getStringExtra( "email" );
 
-        FirstNameTextiView = findViewById(R.id.first_Name_TextView);
-        EmailTextView = findViewById(R.id.email_TextView);
-        PhoneTextView = findViewById(R.id.phone_TextView);
-        BirthDateTextView = findViewById(R.id.birthDate_TextView);
-
-        logOut.setOnClickListener(new View.OnClickListener() {
+        FirstNameTextiView = findViewById( R.id.first_Name_TextView );
+        EmailTextView = findViewById( R.id.email_TextView );
+        PhoneTextView = findViewById( R.id.phone_TextView );
+        BirthDateTextView = findViewById( R.id.birthDate_TextView );
 
 
+        userRef.addValueEventListener( new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()) {
+
+                    String userName = dataSnapshot.child("name").getValue().toString();
+                    String email = dataSnapshot.child("email").getValue().toString();
+                    String birthdate = dataSnapshot.child("birthdate").getValue().toString();
+                    String phone = dataSnapshot.child("phone").getValue().toString();
+
+                FirstNameTextiView.setText("Welcome to your profile, " + userName +"!");
+                EmailTextView.setText("EMAIL: " + email);
+                PhoneTextView.setText("CONTACT NUM: " + phone);
+                    BirthDateTextView.setText("DOB: " + birthdate);
 
 
-                userRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                        if (dataSnapshot.exists()) {
-
-                            String userName = dataSnapshot.child("name").getValue().toString();
-                            String email = dataSnapshot.child("email").getValue().toString();
-                            String birthdate = dataSnapshot.child("birthdate").getValue().toString();
-                            String phone = dataSnapshot.child("phone").getValue().toString();
-
-                            FirstNameTextiView.setText("Welcome to your profile, " + userName + "!");
-                            EmailTextView.setText("EMAIL: " + email);
-                            PhoneTextView.setText("CONTACT NUM: " + phone);
-                            BirthDateTextView.setText("DOB: " + birthdate);
-
-
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
+                }
 
             }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-        });
+            }
+        } );
+
+
+
+
+
     }
+
+
+
+
 }
-
-
-
