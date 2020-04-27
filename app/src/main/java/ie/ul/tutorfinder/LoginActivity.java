870c237587344c518cdc41accee3cc4b0f740a9e
Archit Khanna <architk.222@gmail.com>
2020-04-27 12:16:13 +0100

    Singup button bug Fix
    - Updated Android Manifest File;

--- app/src/main/java/ie/ul/tutorfinder/LoginActivity.java
+++ app/src/main/java/ie/ul/tutorfinder/LoginActivity.java
@@ -2,7 +2,6 @@ package ie.ul.tutorfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +15,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-    Toolbar toolbar;
     EditText userPass;
     EditText userEmail;
     Button userLogin;
