7003859d7c851016742ddcf055f251c70f3ff384
Archit Khanna <architk.222@gmail.com>
2020-04-30 15:50:37 +0100

    Settings Activity & Layout

--- app/src/main/java/ie/ul/tutorfinder/ProfileSettings.java
+++ app/src/main/java/ie/ul/tutorfinder/ProfileSettings.java
@@ -1,14 +1,71 @@
 package ie.ul.tutorfinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileSettings extends AppCompatActivity {
+
+    private DatabaseReference mDatabaseReference;
+    private FirebaseUser mCurrentUser;
+
+    public void openNameDialog(){
+
+    }
+
+    public void openNumberDialog(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
+
+        String user_id = mCurrentUser.getUid();
+
+        Button changeName = findViewById(R.id.chngNamebtn);
+        Button changeNumber = findViewById(R.id.chngPNumbtn);
+
+        changeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNameDialog();
+            }
+        });
+
+        changeNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNumberDialog();
+            }
+        });
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
